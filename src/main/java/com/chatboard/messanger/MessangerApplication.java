d7d5f24ce7aeea0d87dc011b5139fc42385b3297
Maksim Tarasov <mrmade6@gmail.com>
2026-08-26 09:56:57 +0300

    Создание точки входа

---
+++ src/main/java/com/chatboard/messanger/MessangerApplication.java
@@ -0,0 +1,11 @@
+package com.chatboard.messanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessangerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessangerApplication.class, args);
+    }
+}
